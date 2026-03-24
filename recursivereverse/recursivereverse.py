706828ea55d4d41c7633227a6ff04377b28d6569
pavlikravlikk <krailo.pn@ucu.edu.ua>
2026-03-24 16:12:03 +0200

    Update recursivereverse.py

--- recursivereverse/recursivereverse.py
+++ recursivereverse/recursivereverse.py
@@ -1,1 +1,15 @@
+class Node(object):
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
 
+def reverse(head):
+    if head is None or head.next is None:
+        return head
+        
+    rest = reverse(head.next)
+    
+    head.next.next = head
+    head.next = None
+    
+    return rest
