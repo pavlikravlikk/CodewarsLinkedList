aa8147da32ba88d391587540d001607b822c8339
pavlikravlikk <krailo.pn@ucu.edu.ua>
2026-03-24 17:23:22 +0200

    Update canyougettheloop.py
    розв'язок

--- canyougettheloop/canyougettheloop.py
+++ canyougettheloop/canyougettheloop.py
@@ -1,1 +1,16 @@
-
+def loop_size(node):
+    slow = node.next
+    fast = node.next.next
+    
+    while slow != fast:
+        slow = slow.next
+        fast = fast.next.next
+        
+    count = 1
+    fast = fast.next
+    
+    while slow != fast:
+        count += 1
+        fast = fast.next
+        
+    return count
