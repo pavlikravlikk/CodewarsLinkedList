55fb65241218b8fc8e4b3182bb461a9f102efd31
pavlikravlikk <krailo.pn@ucu.edu.ua>
2026-03-24 15:36:39 +0200

    Update parselinkedlistfromastring.py
    ітеративний розв'язок починаючи з кінця

--- convertlikedlistintostring/parselinkedlistfromastring/parselinkedlistfromastring.py
+++ convertlikedlistintostring/parselinkedlistfromastring/parselinkedlistfromastring.py
@@ -1,1 +1,8 @@
+from preloaded import Node
 
+def linked_list_from_string(s):
+    parts = s.split(' -> ')  
+    head = None
+    for val in reversed(parts[:-1]):
+        head = Node(int(val), head)       
+    return head
