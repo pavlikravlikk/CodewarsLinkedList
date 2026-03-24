5934d31d28dda5a58580da1402af800a0f8fba03
pavlikravlikk <krailo.pn@ucu.edu.ua>
2026-03-24 16:22:46 +0200

    Update swapnodepairsinlinkedlist.py
    розв'язок

--- swapnodepairsinlinkedlist/swapnodepairsinlinkedlist.py
+++ swapnodepairsinlinkedlist/swapnodepairsinlinkedlist.py
@@ -1,1 +1,24 @@
+from preloaded import Node
 
+def swap_pairs(head):
+    if not head or not head.next:
+        return head
+    
+    new_head = head.next
+    prev = None
+    curr = head
+    
+    while curr and curr.next:
+        first = curr
+        second = curr.next
+        
+        first.next = second.next
+        second.next = first
+        
+        if prev:
+            prev.next = second
+            
+        prev = first
+        curr = first.next
+        
+    return new_head
